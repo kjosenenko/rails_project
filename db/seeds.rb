87914ec5436a6304e96a0d47b2942115ae050204
KJ Osenenko <karljosenenko@gmail.com>
2021-02-08 12:41:00 -0700

    added date for all models to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
-User.destroy_all
-Listing.destroy_all
+Message.destroy_all
+Participant.destroy_all
 Bid.destroy_all
+Review.destroy_all
+Skill.destroy_all
+Listing.destroy_all
+Conversation.destroy_all
+User.destroy_all
 
 leo = User.create(:username => "Leo", :password => "password")
 ralph = User.create(:username => "Ralph", :password => "password")
@@ -39,3 +44,72 @@ Bid.create(:amount => 400, :message => "You can't beat this offer!", user:donnie
 Bid.create(:amount => 1200, :message => "Have you seen my other work?", user:ralph, listing:roofer)
 Bid.create(:amount => 700, :message => "How soon do you need it done?", user:mikey, listing:sound)
 Bid.create(:amount => 1000, :message => "I can start tomorrow.", user:donnie, listing:sound)
+
+roofer = Skill.create(:name => "Roofer")
+roofer.reviews.build(user:mikey, :author => "Leo", :comment => "Mikey is a great roofer!")
+roofer.save
+plumber = Skill.create(:name => "Plumber")
+plumber.reviews.build(user:leo, :author => "Ralph", :comment => "Leo is a great plumber!")
+plumber.save
+electrician = Skill.create(:name => "Electrician")
+electrician.reviews.build(user:donnie, :author => "Mikey", :comment => "Donnie is a great electrician!")
+electrician.save
+builder = Skill.create(:name => "Builder")
+builder.reviews.build(user:ralph, :author => "Donnie", :comment => "Ralph is a great builder!")
+builder.save
+carpenter = Skill.create(:name => "Carpenter")
+carpenter.reviews.build(user:mikey, :author => "Ralph", :comment => "Mikey is a great carpenter")
+carpenter.save
+musician = Skill.create(:name => "Musician")
+musician.reviews.build(user:leo, :author => "Donnie", :comment => "Leo is a great musician")
+musician.save
+photographer = Skill.create(:name => "Photographer")
+photographer.reviews.build(user:donnie, :author => "Mikey", :comment => "Donnie is a great photographer")
+photographer.save
+chauffeur = Skill.create(:name => "Chauffeur")
+chauffeur.reviews.build(user:ralph, :author => "Leo", :comment => "Ralph is a great chauffeur")
+chauffeur.save
+
+a = Conversation.create
+Participant.create(user:leo, conversation:a)
+Participant.create(user:ralph, conversation:a)
+Message.create(:message => "Hey can you do this job?", :sender => "Leo", conversation:a)
+Message.create(:message => "Sure cabn!", :sender => "Ralph", conversation:a)
+Message.create(:message => "Thank you.", :sender => "Leo", conversation:a)
+
+b = Conversation.create
+Participant.create(user:leo, conversation:b)
+Participant.create(user:mikey, conversation:b)
+Message.create(:message => "Hey can you do this job?", :sender => "Leo", conversation:b)
+Message.create(:message => "Sure cabn!", :sender => "Mikey", conversation:b)
+Message.create(:message => "Thank you.", :sender => "Leo", conversation:b)
+
+c = Conversation.create
+Participant.create(user:leo, conversation:c)
+Participant.create(user:donnie, conversation:c)
+Message.create(:message => "Hey can you do this job?", :sender => "Leo", conversation:c)
+Message.create(:message => "Sure cabn!", :sender => "Donnie", conversation:c)
+Message.create(:message => "Thank you.", :sender => "Leo", conversation:c)
+
+d = Conversation.create
+Participant.create(user:ralph, conversation:d)
+Participant.create(user:mikey, conversation:d)
+Message.create(:message => "Hey can you do this job?", :sender => "Mikey", conversation:d)
+Message.create(:message => "Sure cabn!", :sender => "Ralph", conversation:d)
+Message.create(:message => "Thank you.", :sender => "Mikey", conversation:d)
+
+e = Conversation.create
+Participant.create(user:ralph, conversation:e)
+Participant.create(user:donnie, conversation:e)
+Message.create(:message => "Hey can you do this job?", :sender => "Donnie", conversation:e)
+Message.create(:message => "Sure cabn!", :sender => "Ralph", conversation:e)
+Message.create(:message => "Thank you.", :sender => "Donnie", conversation:e)
+
+f = Conversation.create
+Participant.create(user:mikey, conversation:f)
+Participant.create(user:donnie, conversation:f)
+Message.create(:message => "Hey can you do this job?", :sender => "Mikey", conversation:f)
+Message.create(:message => "Sure cabn!", :sender => "Donnie", conversation:f)
+Message.create(:message => "Thank you.", :sender => "Mikey", conversation:f)
+
+
--- spec.md
+++ spec.md
@@ -14,7 +14,7 @@ Specs:
         review.comment
 - [x] Include reasonable validations for simple model objects (list of model objects with validations e.g. User, Recipe, Ingredient, Item)
         models validate: "presence", "uniqueness"
-- [ ] Include a class level ActiveRecord scope method (model object & class method name and URL to see the working feature e.g. User.most_recipes URL: /users/most_recipes)
+- [x] Include a class level ActiveRecord scope method (model object & class method name and URL to see the working feature e.g. User.most_recipes URL: /users/most_recipes)
         Search for "like words in listing title"
 - [x] Include signup (how e.g. Devise)
         User.signup
